60dbbf211e5895918b628441d0d3a3c065578ac4
Wesley Charles <123204710+B00rges@users.noreply.github.com>
2023-12-04 20:58:15 -0300

    to-do-list
    refatoração de código

--- app.js
+++ app.js
@@ -1,47 +1,72 @@
-
-const formAddTodo = document.querySelector('.form-add-todo')
-const todoContainer = document.querySelector('.todos-container')
+const formAddTodo  = document.querySelector('.form-add-todo')
+const todosContainer = document.querySelector('.todos-container')
 const inputSearchTodo = document.querySelector('.form-search input')
+
+const addTodo = inputValue => {
+    if (inputValue.length) {
+      todosContainer.innerHTML += `
+        <li class="list-group-item d-flex justify-content-between align-items-center" data-todo="${inputValue}">
+         <span>${inputValue}</span>
+         <i class="far fa-trash-alt" data-trash="${inputValue}"></i>
+        </li>
+     `
+    
+        event.target.reset()
+    }
+}
 
 formAddTodo.addEventListener('submit', event => {
     event.preventDefault()
 
    const inputValue = event.target.add.value.trim()
+   addTodo(inputValue)
 
-  if(inputValue.length) {
-    todoContainer.innerHTML += `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-    <span>${inputValue}</span>
-    <i class="far fa-trash-alt delete"></i>
-    </li>
-    `  
-    event.target.reset() 
-  }
 })
 
-todoContainer.addEventListener('click', event => {
+const removeTodo = clickElement => {
+    const trashDataValue = clickElement.dataset.trash
+    const todo = document.querySelector(`[data-todo="${trashDataValue}"]`)
 
     const clickElement = event.target
-
-if(Array.from(clickElement.classList).includes('delete')) {
-    clickElement.parentElement.remove()
+    if (trashDataValue) {
+        todo.remove()
+    }
 }
+
+const filterTodos = (todos, inputValue, returnMatchedTodos) => todos 
+    .filter(todo => {
+        const matchedTodos = todo.textContent.toLowerCase().includes(inputValue)
+        return returnMatchedTodos ? matchedTodos : !matchedTodos
+    })
+
+const maniupateClasses = (todos, classToAdd, classToRemove) => {
+    todos.forEach(todo => {
+        todo.classList.remove(classToRemove)
+        todo.classList.add(classToAdd)
+    }) 
+}
+
+const hideTodos = (todos, inputValue) => {
+   const todosToHide = filterTodos(todos, inputValue, false)
+   maniupateClasses(todosToHide, 'hidden', 'd-flex')
+}
+
+const showTodos = (todos, inputValue) => {
+    const todosToShow = filterTodos(todos, inputValue, true)
+    maniupateClasses(todosToShow, 'd-flex', 'hidden')
+}
+
+todosContainer.addEventListener('click', event => {
+    const clickElement = event.target
+    removeTodo(clickElement)
 })
 
-
 inputSearchTodo.addEventListener('input', event => {
-    const inputValue = event.target.value.trim().toLowerCase()
-    Array.from(todoContainer.children)
-        .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
-        .forEach(todo => {
-            todo.classList.remove('d-flex')
-            todo.classList.add('hidden')
-        })
+    const inputValue = event.target.value
+    const todos = Array.from(todosContainer.children)
+  
+    hideTodos(todos, inputValue)
+    showTodos(todos, inputValue)
 
-    Array.from(todoContainer.children)
-        .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
-        .forEach(todo => {
-            todo.classList.remove('hidden')
-            todo.classList.add('d-flex')
-        })
+
 })
